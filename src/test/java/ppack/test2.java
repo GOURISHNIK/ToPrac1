09009649d3f915e7107048c5c5f521f4f458ab0d
GOURISHNIK <gourishnik@gmail.com>
2022-10-06 13:12:22 +0530

    this is 1

--- src/test/java/ppack/test2.java
+++ src/test/java/ppack/test2.java
@@ -1,6 +1,17 @@
 package ppack;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
 public class test2 {
 	//9:37pm
+	@Test
+	public String getCurrentTime() {
+		String currentDate = new SimpleDateFormat("yyyy-MM-dd-hhmmss").format(new Date());
+		System.out.println(currentDate);
+		return currentDate;
+	}
 
 }
